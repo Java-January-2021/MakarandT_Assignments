5e8e0b1c177b15c98f2a77195cc5bc003f16d1e9
Mtankhiwale <Makarand.Tankiwale@verint.com>
2021-01-13 06:33:57 -0800

    FizzBuzz Assignment

---
+++ 01-java-fundamentals/03-fizzbuzz/FizzbuzzController.java
@@ -0,0 +1,11 @@
+public class FizzbuzzController {
+    public static void main(String[] args) {
+        FizzBuzz a = new FizzBuzz();
+        System.out.println(a.fizzBuzz(3));
+        System.out.println(a.fizzBuzz(5));
+        System.out.println(a.fizzBuzz(15));
+        System.out.println(a.fizzBuzz(2));
+
+    }
+
+}
