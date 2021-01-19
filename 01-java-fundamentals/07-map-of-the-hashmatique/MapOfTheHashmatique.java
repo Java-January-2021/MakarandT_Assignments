518d245542a2b7cb6876198046d436d187b7e780
Mtankhiwale <Makarand.Tankiwale@verint.com>
2021-01-19 02:26:01 -0800

    Map-Of-the-hashmatique assignment

---
+++ 01-java-fundamentals/07-map-of-the-hashmatique/MapOfTheHashmatique.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Set;
+
+public class MapOfTheHashmatique {
+
+    public static void main(String[] args) {
+        
+   
+    HashMap<String, String> trackList = new HashMap<String, String>();
+    trackList.put("Tired To Death","sample lyrics1");
+    trackList.put("Questo Mostrarsi","sample lyrics2");
+    trackList.put("Abiata","sample lyrics3");
+    trackList.put("From the Fog","sample lyrics4");
+    Set<String> keys = trackList.keySet();
+    for (String key : keys) {
+        System.out.println(key);
+        System.out.println(trackList.get(key));
+    }
+    System.out.println("containsKey="+ trackList.containsKey("Abiata"));
+    System.out.println("Pull out one of the songs by its track title="+"containsKey="+ trackList.get("Abiata"));for (String key : keys) {
+        System.out.print(key);
+        System.out.println(":"+trackList.get(key));
+    }
+}
+ }
