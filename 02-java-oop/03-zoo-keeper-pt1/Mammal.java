e04dcfac8a2fa47a209fa3f0fdd34b70807a3ad8
Mtankhiwale <Makarand.Tankiwale@verint.com>
2021-01-23 05:10:23 -0800

    Zookeeper 1 fix mammal

--- 02-java-oop/03-zoo-keeper-pt1/Mammal.java
+++ 02-java-oop/03-zoo-keeper-pt1/Mammal.java
@@ -1,6 +1,5 @@
 class Mammal {
     public int energyLevel;
-
     // Create a Mammal class that has an energyLevel and displayEnergy() method. The
     // displayEnergy() method should show the animals energy level as well as return
     // it.
@@ -12,18 +11,4 @@ class Mammal {
         System.out.println(energyLevel);
         return energyLevel;
     }
-
-    // Getter
-    public int getEnergyLevel() {
-        return energyLevel;
-    }
-
-    // Setter
-    public void setEnergyLevel() {
-        this.energyLevel = energyLevel;
-    }
-
-  
-
-   
 }
