1255290dea4cf1c93ced9fcfda6476e0f4833f9e
Mtankhiwale <Makarand.Tankiwale@verint.com>
2021-01-26 06:46:20 -0800

    cal ii

---
+++ 02-java-oop/11-calculator-ii-optional/CalculatorTest.java
@@ -0,0 +1,28 @@
+public class CalculatorTest {
+
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        cal.setOperandOne(10.5);
+        System.out.println(cal.getOperandOne());
+        cal.setOperation("+");
+        System.out.println(cal.getOperation());
+        cal.setOperandTwo(5.2);
+        System.out.println(cal.getOperandTwo());
+        cal.performOperation();
+        System.out.println(cal.getResults());
+        performOperation(10.5)
+
+        performOperation("+")
+            
+        performOperation(5.2)
+            
+        performOperation("*")
+            
+        performOperation(10)
+            
+        performOperation("=")
+            
+        getResults()
+
+    }
+}
