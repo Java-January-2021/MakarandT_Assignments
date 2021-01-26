3ebb73b80626a979d26fd7296db8886c7cb514fe
Mtankhiwale <Makarand.Tankiwale@verint.com>
2021-01-25 20:36:54 -0800

    calculator I

---
+++ 02-java-oop/10-calculator-i/Calculator.java
@@ -0,0 +1,50 @@
+
+public class Calculator {
+
+    private double OperandOne;
+    private double OperandTwo;
+    private String Operation;
+    private Double results;
+
+    public Calculator() {
+    }
+
+    public double getOperandOne() {
+        return OperandOne;
+    }
+
+    public void setOperandOne(double operandOne) {
+        this.OperandOne = operandOne;
+    }
+
+    public double getOperandTwo() {
+        return OperandTwo;
+    }
+
+    public void setOperandTwo(double operandTwo) {
+        this.OperandTwo = operandTwo;
+    }
+
+    public String getOperation() {
+        return this.Operation;
+    }
+
+    public void setOperation(String operation) {
+        Operation = operation;
+    }
+
+    public void performOperation() {
+        if (this.Operation == "+") {
+            results = OperandOne + OperandTwo;
+        }
+        if (this.Operation == "-") {
+            results = OperandOne - OperandTwo;
+        }
+
+    }
+
+    public Double getResults() {
+        return results;
+    }
+
+}
---
+++ 02-java-oop/10-calculator-i/CalculatorTest.java
@@ -0,0 +1,15 @@
+public class CalculatorTest {
+
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        cal.setOperandOne(10.5);
+        System.out.println(cal.getOperandOne());
+        cal.setOperation("+");
+        System.out.println(cal.getOperation());
+        cal.setOperandTwo(5.2);
+        System.out.println(cal.getOperandTwo());
+        cal.performOperation();
+        System.out.println(cal.getResults());
+
+    }
+}
